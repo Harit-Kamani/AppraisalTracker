9bd733c6d7afaa7adca6b8249712fb8fdbd180f1
Harit <kamaniharit77@gmail.com>
2025-02-07 17:17:51 +0530

    Init

---
+++ src/main/java/com/grownited/AppraisalTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.grownited;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppraisalTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppraisalTrackerApplication.class, args);
+	}
+
+}
